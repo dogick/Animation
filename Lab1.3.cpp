fddeff7e623dabd1fee0626f9563b108cd62a133
dogick <vadim.dolganoff2014@gmail.com>
2016-10-23 12:02:11 +0300

    Add files via upload

--- Lab1.3.cpp
+++ Lab1.3.cpp
@@ -10,12 +10,18 @@ static const int WINDOW_WIDTH = 700;
 static const int DISTANCE_BETWEEN_BLOCKS = 30;
 static const sf::Vector2f POSITION_CENTER_BLOCKS = {340, 270.f };
 static const float PI = 3.14159f;
+static const float STEP_ANGLE = 0.01f;
+static const int CENTER_ELEMENT_INDEX = 4;
+static const int MAX_ANGLE = 90;
+static const sf::Color BLOCK_COLOR = sf::Color(102.f, 0, 102.f);
 
 enum class Step
 {
 	FIRST,
 	SECOND,
 	THIRD,
+	FOURTH,
+	FIFTH,
 };
 
 struct Rectangle
@@ -24,7 +30,7 @@ struct Rectangle
 	sf::RectangleShape shape;
 	float currentAngle;
 	int index;
-	bool isRotation;
+	float transparent;
 };
 
 typedef std::vector<Rectangle*> Rectangles;
@@ -35,10 +41,10 @@ Rectangle::Rectangle(sf::Vector2f const& position, sf::Vector2f const& size, sf:
 	shape.setSize(size);
 	shape.setOrigin(size.x/2, size.y/2);
 	shape.setScale(scale);
-	shape.setFillColor(sf::Color(102, 0, 102));
+	shape.setFillColor(sf::Color(BLOCK_COLOR));
 	currentAngle = 0;
 	index = i;
-	isRotation = false;
+	transparent = 255;
 }
 
 void AnimateFirst(float elapsedTime, Rectangle* rectangle, Step & step)
@@ -59,15 +65,17 @@ void AnimateSecond(float elapsedTime, Rectangle* rectangle, Step & step)
 {
 	sf::Vector2f position = rectangle->shape.getPosition();
 	sf::Vector2f size = rectangle->shape.getSize();
+	float speed = SPEED * elapsedTime;
+	
 	if (int(position.y) != POSITION_CENTER_BLOCKS.y)
 	{
-		if (rectangle->index >= 5)
+		if (rectangle->index >= CENTER_ELEMENT_INDEX)
 		{
-			rectangle->shape.move(-SPEED * elapsedTime, -SPEED * elapsedTime);
+			rectangle->shape.move(-speed, -speed);
 		}
-		else if(rectangle->index <= 5)
+		else if(rectangle->index <= CENTER_ELEMENT_INDEX)
 		{
-			rectangle->shape.move(SPEED * elapsedTime, SPEED * elapsedTime);
+			rectangle->shape.move(speed, speed);
 		}
 	}
 	else if(rectangle->index == MAX_COUNT_BLOCK - 1)
@@ -76,37 +84,13 @@ void AnimateSecond(float elapsedTime, Rectangle* rectangle, Step & step)
 	}
 }
 
-
-/*void AnimateFourth(float elapsedTime, Rectangle* rectangle, Step & step, sf::Vector2f & center)
-{
-	sf::Vector2f position = rectangle->shape.getPosition();
-	sf::Vector2f size = rectangle->shape.getSize();
-	if (int(rectangle->currentAngle) != 45)
-	{
-		float dx = center.x - position.x;
-		float dy = center.y - position.y;
-		float distance = hypot(dx, dy);
-		if (rectangle->index == 4)
-		{
-		//	std::cout << rectangle->shape.getPosition().x << std::endl;
-		}
-		float vx = cos(rectangle->currentAngle) * distance;
-		float vy = sin(rectangle->currentAngle) * distance;
-		rectangle->shape.setRotation(rectangle->currentAngle);
-
-		rectangle->shape.setPosition(vx + center.x, vy+ center.y);
-		rectangle->currentAngle += 0.1;
-	}
-}*/
-
-
 void AnimateThird(Rectangle* rectangle, Step & step, sf::Vector2f & center)
 {
 	sf::Vector2f position = rectangle->shape.getPosition();
 	float dx = center.x - position.x;
 	float dy = center.y - position.y;
 	float distance = hypot(dx, dy);
-	if (rectangle->index > 4)
+	if (rectangle->index > CENTER_ELEMENT_INDEX)
 	{
 		distance *= -1;
 	}
@@ -114,40 +98,84 @@ void AnimateThird(Rectangle* rectangle, Step & step, sf::Vector2f & center)
 	float vx = cos(rectangle->currentAngle) * distance;
 	float vy = sin(rectangle->currentAngle) * distance;
 	rectangle->shape.setPosition(vx + center.x, vy+ center.y);
-	rectangle->currentAngle += 0.01f;
+	rectangle->currentAngle += STEP_ANGLE;
+
+}
+
+void AnimateFouth(float elapsedTime, Rectangle* rectangle, Step & step, sf::Vector2f & center)
+{
+	sf::Vector2f position = rectangle->shape.getPosition();
+	sf::Vector2f size = rectangle->shape.getSize();
+	float speed = SPEED * elapsedTime;
+	if (int(position.x) <= 600)
+	{
+		rectangle->shape.setFillColor(sf::Color(102.f, 0, 102.f, rectangle->transparent));
+		rectangle->transparent = rectangle->transparent - 0.05;
+		rectangle->shape.move(speed, speed);
+	}
+	else
+	{
+		step = Step::FIFTH;
+	}
+}
+
+void AnimateFifth(float elapsedTime, Rectangle* rectangle, Step & step, sf::Vector2f & center)
+{
+	sf::Vector2f scale = rectangle->shape.getScale();
+	sf::Vector2f position = rectangle->shape.getPosition();
+	sf::Vector2f size = rectangle->shape.getSize();
+	float speed = SPEED * elapsedTime;
+	if (int(position.x) >= 200)
+	{
+		if (scale.x >= 0.2f)
+		{
+			rectangle->shape.setScale(scale.x - 0.001, scale.x - 0.001);
+		}
+		else if(scale.x <= 1.f)
+		{
+			std::cout << "dfg" << std::endl;
+			rectangle->shape.setScale(scale.x + 0.001, scale.x + 0.001);
+		}
+		rectangle->shape.move(-speed, 0);
+	}
+	else
+	{
+		step = Step::FIFTH;
+	}
 }
 
 void Update(float elapsedTime, Rectangles & rectangles, Step & step)
 {
-	sf::Vector2f origin = rectangles[4]->shape.getOrigin();
 	sf::Vector2f center = rectangles[4]->shape.getPosition();
-	std::cout << center.x << " | " << center.y << std::endl;
-	if (step != Step::THIRD)
+	for (auto rectangle : rectangles)
 	{
-		for (auto rectangle : rectangles)
+		if ((step == Step::THIRD) && (rectangle->shape.getRotation() <= MAX_ANGLE)) //По элементная анимация 
 		{
-			switch (step)
-			{
-			case Step::FIRST:
-				AnimateFirst(elapsedTime, rectangle, step);
-				break;
-			case Step::SECOND:
-				AnimateSecond(elapsedTime, rectangle, step);
-				break;
-			default:
-				break;
-			}
+			AnimateThird(rectangle, step, center);
+			break;
 		}
-	}
-	else
-	{
-		for(auto rectangle : rectangles)
+		else if ((step == Step::THIRD) && (rectangle->index == 8) && (rectangle->shape.getRotation() >= MAX_ANGLE))
 		{
-			if (rectangle->shape.getRotation() <= 90)
-			{
-				AnimateThird(rectangle, step, center);
-				break;
-			}
+			step = Step::FOURTH;
+		}
+
+		
+		switch (step)
+		{
+		case Step::FIRST:
+			AnimateFirst(elapsedTime, rectangle, step);
+			break;
+		case Step::SECOND:
+			AnimateSecond(elapsedTime, rectangle, step);
+			break;
+		case Step::FOURTH:
+			AnimateFouth(elapsedTime, rectangle, step, center);
+			break;
+		case Step::FIFTH:
+			AnimateFifth(elapsedTime, rectangle, step, center);
+			break;
+		default:
+			break;
 		}
 	}
 }
@@ -175,138 +203,37 @@ void Render(sf::RenderWindow & window, Rectangles const& rectangles, float elaps
 	window.display();
 }
 
-int main()
+void CreateRectangles(Rectangles & rectangles)
 {
-	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML!");
-	Rectangles rectangles;
 	float stepRec = 0;
 	for (int i = 0; i != MAX_COUNT_BLOCK; ++i)
 	{
 		rectangles.push_back(new Rectangle(sf::Vector2f(0, 150 + stepRec), sf::Vector2f(20, 20), sf::Vector2f(1, 1), i));
 		stepRec += DISTANCE_BETWEEN_BLOCKS;
 	}
-	sf::Clock clock;
-
-	Step step = Step::FIRST;
-	while (window.isOpen())
-	{
-		float elapsedTime = clock.getElapsedTime().asMicroseconds(); 
-		clock.restart(); 
-		elapsedTime = elapsedTime / 800; 
-		HandleEvents(window);
-		Update(elapsedTime, rectangles, step);
-		Render(window, rectangles, elapsedTime);
-	}
-
-	return 0;
-}
-/*class CRectangle
-{
-public:
-	CRectangle(sf::Vector2f const& position, sf::Vector2f const& size, sf::Vector2f const& scale, Step const& direction);
-	void Update(float elapsedTime);
-	void Draw(sf::RenderWindow & window);
-	void GenerateRectangle();
-private:
-	sf::RectangleShape m_rectangle;
-	Step m_direction;
-	void AnimateRight(float elapsedTime);
-	void AnimateDiagonally();
-};
-
-void CRectangle::GenerateRectangle()
-{
-
 }
 
-CRectangle::CRectangle(sf::Vector2f const& position, sf::Vector2f const& size, sf::Vector2f const& scale, Step const& direction)
+float GetElapsedTime(sf::Clock & clock)
 {
-	m_rectangle.setPosition(position);
-	m_rectangle.setSize(size);
-	m_rectangle.setOrigin(size.x / 2, size.y / 2);
-	m_rectangle.setScale(scale);
-	m_rectangle.setFillColor(sf::Color(102, 0, 102));
-	m_direction = direction;
-}
-
-void CRectangle::AnimateRight(float elapsedTime)
-{
-	sf::Vector2f position = m_rectangle.getPosition();
-	sf::Vector2f size = m_rectangle.getSize();
-
-	if (position.x + size.x / 2 <= WINDOW_WIDTH)
-	{
-		//m_rectangle.setRotation(m_rectangle.getRotation() + 0.1);
-		m_rectangle.move(SPEED * elapsedTime, 0);
-	}
-}
-
-void CRectangle::AnimateDiagonally()
-{
-
-}
-
-void CRectangle::Update(float elapsedTime)
-{
-	switch (m_direction)
-	{
-	case Step::FIRST:
-		AnimateRight(elapsedTime);
-		break;
-
-	default:
-		break;
-	}
-}
-
-void CRectangle::Draw(sf::RenderWindow & window)
-{
-	window.draw(m_rectangle);
-}
-
-void Render(sf::RenderWindow & window, std::vector<CRectangle*> & rectangles, float elapsedTime)
-{
-	window.clear(sf::Color::White);
-	for (auto rectangle : rectangles)
-	{
-		rectangle->Update(elapsedTime);
-		rectangle->Draw(window);
-	}
-	window.display();
-}
-
-void HandleEvents(sf::RenderWindow & window)
-{
-	sf::Event event;
-	while (window.pollEvent(event))
-	{
-		if (event.type == sf::Event::Closed)
-		{
-			window.close();
-		}
-	}
+	float elapsedTime = clock.getElapsedTime().asMicroseconds();
+	clock.restart();
+	return elapsedTime / 800;
 }
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML!");
-	std::vector<CRectangle*> rectangles;
-	float step = 0;
-	for (int i = 0; i != MAX_COUNT_BLOCK; ++i)
-	{
-		step += 50;
-		rectangles.push_back(new CRectangle(sf::Vector2f(0, 100 + step) , sf::Vector2f(30, 30), sf::Vector2f(1, 1), Step::FIRST));
-	}
+	Rectangles rectangles;
+	CreateRectangles(rectangles);
 
 	sf::Clock clock;
+	Step step = Step::FIRST;
 	while (window.isOpen())
 	{
-		float elapsedTime = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
-		clock.restart(); //перезагружает время
-		elapsedTime = elapsedTime / 800; //скорость игры
+		float elapsedTime = GetElapsedTime(clock);
 		HandleEvents(window);
-		Render(window, rectangles, elapsedTime);
-			
+		Update(elapsedTime, rectangles, step);
+		Render(window, rectangles, elapsedTime);	
 	}
 
 	return 0;
-}*/
+}
